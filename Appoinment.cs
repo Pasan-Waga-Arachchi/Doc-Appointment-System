a7ec2ac9fb1dab23e28adb3a64fe9ac55809ea5f
Lahiru Prasanga <lahiruprasangasrimal@gmail.com>
2023-09-12 01:18:43 +0530

    BasicClassesSecondUpdate_02

---
+++ Appoinment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAD_Project.BasicClasses
+{
+    class Appoinment
+    {
+        public int AppoinmentId { get; set; }
+        public string PatientId{ get; set; }
+        public string DoctorId { get; set; }
+        public DateTime AppoinmentDate { get; set; }
+        public DateTime AppoinmentCreatedDate { get; set; }
+        public string AppoinmentStatus { get; set; }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAD_Project.BasicClasses
+{
+    abstract class User
+    {
+
+       public string Name { get; set; }
+        public string Password { get; set; }
+        public string ID { get; set; }
+        public string gender { get; set; }
+        protected string UserType { get; set; }
+    }
+}
